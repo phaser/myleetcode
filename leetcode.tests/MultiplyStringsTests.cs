c53da3ee8126b471c3dc6f2d8ef12e99dfa3d0c3
Cristian Bidea <cristian.bidea@gmail.com>
2026-07-01 20:39:56 +0300

    solved multiply

--- leetcode.tests/MultiplyStringsTests.cs
+++ leetcode.tests/MultiplyStringsTests.cs
@@ -15,4 +15,41 @@ public class MultiplyStringsTests
         solver = new MultiplyStringsSolver(num1, num2);
         Assert.Equal("56088", solver.Multiply());
     }
+
+    [Fact]
+    public void TestBuggyMultiply()
+    {
+        var num1 = "9";
+        var num2 = "99";
+        var solver = new MultiplyStringsSolver(num1, num2);
+        var result = solver.Multiply();
+        Assert.Equal("891", result);
+    }
+    
+    [Fact]
+    public void TestMultiplyByChar()
+    {
+        var num1 = "98123";
+        char num2 = '7';
+        
+        var solver = new MultiplyStringsSolver(num1, num1);
+        var result = MultiplyStringsSolver.Multiply(num1.Reverse().ToArray(), num2);
+        //686861
+        Assert.Equal("168686", result);
+    }
+
+    [Fact]
+    public void TestAdd()
+    {
+        char[] num1 = [];
+        char[] num2 = ['3', '2', '1', '9'];
+        var solver = new MultiplyStringsSolver("", "");
+        var r = MultiplyStringsSolver.Add(num1, num2, 0);
+        var r1 = MultiplyStringsSolver.Add(num1, num2, 1);
+        var r2 = MultiplyStringsSolver.Add(num1, num2, 2);
+        
+        Assert.Equal(['3', '2', '1', '9'], r);
+        Assert.Equal(['0', '3', '2', '1', '9'], r1);
+        Assert.Equal(['0', '0', '3', '2', '1', '9'], r2);
+    }
 }
--- leetcode/MultiplyStrings.cs
+++ leetcode/MultiplyStrings.cs
@@ -4,21 +4,77 @@ namespace leetcode;
 
 public class MultiplyStringsSolver(string num1, string num2)
 {
-    private readonly char[] _num1 = num1.Reverse().ToArray() ?? throw new ArgumentException("null not allowed");
-    private readonly char[] _num2 = num2.Reverse().ToArray() ?? throw new ArgumentException("null not allowed");
+    private char[] _num1 = num1.Reverse().ToArray() ?? throw new ArgumentException("null not allowed");
+    private char[] _num2 = num2.Reverse().ToArray() ?? throw new ArgumentException("null not allowed");
 
-    public string Multiply()
+    public static string Multiply(char[] reversedNum, char c)
     {
         var result = new StringBuilder();
         var carry = 0;
+        foreach (var t in reversedNum)
+        {
+            var d = (c - 48) * (t - 48) + carry;
+            carry = d / 10;
+            d = d % 10;
+            result.Append((char)(d + 48));
+        }
+
+        if (carry > 0)
+            result.Append((char)(48 + carry));
+        return result.ToString();
+    }
+
+    public static char[] Add(char[] num1, char[] num2, int offset)
+    {
+        var ml = Math.Max(num1.Length, num2.Length);
+        var result = new StringBuilder();
+
+        for (var i = 0; i < offset; i++)
+        {
+            result.Append(i < num1.Length ? num1[i] : (char)48);
+        }
+
+        var carry = 0;
+        for (var i = 0; i < ml; i++)
+        {
+            var d1 = offset + i < num1.Length ? num1[offset + i] : 48;
+            var d2 = i < num2.Length ? num2[i] : 48;
+            var d = d1 + d2 - 96 + carry;
+            carry = d / 10;
+            d %= 10;
+            result.Append((char)(d + 48));
+        }
         
-        return new string(result.ToString().Reverse().ToArray());
+        if (carry > 0)
+            result.Append((char)(48 + carry));
+        
+        while (result.Length > 1 && result[result.Length - 1] == 48)
+            result.Remove(result.Length - 1, 1);
+        
+        return result.ToString().ToCharArray();
+    }
+    
+    public string Multiply()
+    {
+        if (num1.Length < num2.Length)
+        {
+            (_num1, _num2) = (_num2, _num1);
+        }
+
+        var results = new List<string>();
+        char[] intr = [];
+        for (var i = 0; i < _num2.Length; i++)
+        {
+            intr = Add(intr, Multiply(_num1, _num2[i]).ToCharArray(), i);
+        }
+        
+        return new string(intr.Reverse().ToArray());
     }
 }
 
-public class Solution {
+public class MultiplyStringsSolution {
     public string Multiply(string num1, string num2)
     {
-        return "";
+        return new MultiplyStringsSolver(num1, num2).Multiply();
     }
 }
