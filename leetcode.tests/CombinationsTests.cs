70d1f07b4fcdb86da9898929bc0887983c92c5e6
Cristian Bidea <cristian.bidea@uipath.com>
2026-07-01 16:38:02 +0300

    solved combinations

--- leetcode.tests/CombinationsTests.cs
+++ leetcode.tests/CombinationsTests.cs
@@ -2,5 +2,16 @@ namespace leetcode.tests;
 
 public class CombinationsTests
 {
-    
+    [Fact]
+    public async Task TestBacktrack()
+    {
+        var solver = new CombinationsSolver(2, 2);
+        var solutions = solver.GetCombinations();
+        Assert.Single(solutions);
+        
+        solver = new CombinationsSolver(4, 2);
+        solutions = solver.GetCombinations();
+        Assert.Equal(6, solutions.Count);
+        Assert.Equal([[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]],  solutions);
+    }
 }
--- leetcode/Combinations.cs
+++ leetcode/Combinations.cs
@@ -1,8 +1,39 @@
 namespace leetcode;
 
+public class CombinationsSolver(int n, int k)
+{
+    private readonly int _n = n;
+    private readonly int _k = k;
+
+    public IList<IList<int>> GetCombinations()
+    {
+        var solutions = new List<IList<int>>();
+        var solution = new int[_k];
+
+        for (var i = 0; i < solution.Length; i++)
+            solution[i] = i + 1;
+        
+        while (true)
+        {
+            solutions.Add(solution.ToList());
+            var i = _k - 1;
+            while (i >= 0 && solution[i] == _n - _k + i + 1)
+                i--;
+
+            if (i < 0)
+                break;
+
+            solution[i]++;
+            for (var j = i + 1; j < _k; j++)
+                solution[j] = solution[j - 1] + 1;
+        }
+        return solutions;
+    }
+}
+
 public class Solution {
     public IList<IList<int>> Combine(int n, int k)
     {
-        return null;
+        return new CombinationsSolver(n, k).GetCombinations();
     }
 }
